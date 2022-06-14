22d295e298819108f1ec4c8614b266a45671e10e
Samuel Marvin <samuelmarvin@Samuels-MacBook-Pro-2.local>
2022-06-14 14:22:07 -0500

    setting up

---
+++ frontEnd/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='navBar'>header</nav>
+      <div className='mainContent'>main</div>
+      <footer className='footer'>footer</footer>
+    </div>
+  );
+}
+
+export default App;
